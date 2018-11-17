dc4d9f096139a53e2aa03847b2d081e557ba8bde
Takayama Fumihiko <tekezo@pqrs.org>
2018-11-17 10:39:18 +0900

    add iokit_usage_page_apple_vendor_*

--- include/pqrs/osx/iokit_types/iokit_usage_page.hpp
+++ include/pqrs/osx/iokit_types/iokit_usage_page.hpp
@@ -17,6 +17,12 @@ struct iokit_usage_page : type_safe::strong_typedef<iokit_usage_page, int32_t>,
   using strong_typedef::strong_typedef;
 };
 
+inline std::ostream& operator<<(std::ostream& stream, const iokit_usage_page& value) {
+  return stream << type_safe::get(value);
+}
+
+// from IOHIDUsageTables.h
+
 constexpr iokit_usage_page iokit_usage_page_generic_desktop = iokit_usage_page(kHIDPage_GenericDesktop);
 constexpr iokit_usage_page iokit_usage_page_simulation = iokit_usage_page(kHIDPage_Simulation);
 constexpr iokit_usage_page iokit_usage_page_vr = iokit_usage_page(kHIDPage_VR);
@@ -49,9 +55,28 @@ constexpr iokit_usage_page iokit_usage_page_magnetic_stripe_reader = iokit_usage
 constexpr iokit_usage_page iokit_usage_page_camera_control = iokit_usage_page(kHIDPage_CameraControl);
 constexpr iokit_usage_page iokit_usage_page_arcade = iokit_usage_page(kHIDPage_Arcade);
 
-inline std::ostream& operator<<(std::ostream& stream, const iokit_usage_page& value) {
-  return stream << type_safe::get(value);
-}
+// from AppleHIDUsageTables.h
+
+constexpr iokit_usage_page iokit_usage_page_apple_vendor = iokit_usage_page(0xff00);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_keyboard = iokit_usage_page(0xff01);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_mouse = iokit_usage_page(0xff02);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_accelerometer = iokit_usage_page(0xff03);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_ambient_light_sensor = iokit_usage_page(0xff04);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_temperature_sensor = iokit_usage_page(0xff05);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_headset = iokit_usage_page(0xff07);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_power_sensor = iokit_usage_page(0xff08);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_smart_cover = iokit_usage_page(0xff09);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_platinum = iokit_usage_page(0xff0A);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_lisa = iokit_usage_page(0xff0B);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_motion = iokit_usage_page(0xff0C);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_battery = iokit_usage_page(0xff0D);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_ir_remote = iokit_usage_page(0xff0E);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_debug = iokit_usage_page(0xff0F);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_ir_interface = iokit_usage_page(0xff10);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_filtered_event = iokit_usage_page(0xff50);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_multitouch = iokit_usage_page(0xff60);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_display = iokit_usage_page(0xff92);
+constexpr iokit_usage_page iokit_usage_page_apple_vendor_top_case = iokit_usage_page(0x00ff);
 } // namespace osx
 } // namespace pqrs
 
