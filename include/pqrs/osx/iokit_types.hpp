4d770f82fb5e0000186a910c1fb33c36560f1994
Takayama Fumihiko <tekezo@pqrs.org>
2020-04-10 19:00:50 +0900

    version 3.6.0

--- include/pqrs/osx/iokit_types.hpp
+++ include/pqrs/osx/iokit_types.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::iokit_types v3.5
+// pqrs::osx::iokit_types v3.6
 
 // (C) Copyright Takayama Fumihiko 2018.
 // Distributed under the Boost Software License, Version 1.0.
