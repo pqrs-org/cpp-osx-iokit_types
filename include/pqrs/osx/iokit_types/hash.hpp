09bb86d8636ed70a781ea560a7718d7f944a54fd
Takayama Fumihiko <tekezo@pqrs.org>
2020-04-21 23:40:08 +0900

    update for pqrs::hash changes

--- include/pqrs/osx/iokit_types/hash.hpp
+++ include/pqrs/osx/iokit_types/hash.hpp
@@ -15,9 +15,9 @@ template <>
 struct hash<std::tuple<pqrs::hid::vendor_id::value_t, pqrs::hid::product_id::value_t, pqrs::osx::iokit_hid_location_id::value_t>> {
   size_t operator()(const std::tuple<pqrs::hid::vendor_id::value_t, pqrs::hid::product_id::value_t, pqrs::osx::iokit_hid_location_id::value_t>& tuple) const {
     size_t h = 0;
-    pqrs::hash_combine(h, type_safe::get(std::get<0>(tuple)));
-    pqrs::hash_combine(h, type_safe::get(std::get<1>(tuple)));
-    pqrs::hash_combine(h, type_safe::get(std::get<2>(tuple)));
+    pqrs::hash::combine(h, type_safe::get(std::get<0>(tuple)));
+    pqrs::hash::combine(h, type_safe::get(std::get<1>(tuple)));
+    pqrs::hash::combine(h, type_safe::get(std::get<2>(tuple)));
     return h;
   }
 };
