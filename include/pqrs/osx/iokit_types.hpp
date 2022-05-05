4a5a54c93a022356c4e19dc7d74852c8b1d21547
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-05 23:01:14 +0900

    Add iokit_mach_port

--- include/pqrs/osx/iokit_types.hpp
+++ include/pqrs/osx/iokit_types.hpp
@@ -9,4 +9,5 @@
 #include "iokit_types/hash.hpp"
 #include "iokit_types/iokit_hid_location_id.hpp"
 #include "iokit_types/iokit_keyboard_type.hpp"
+#include "iokit_types/iokit_mach_port.hpp"
 #include "iokit_types/iokit_registry_entry_id.hpp"
