8054e18150df8030b0d9a1a819a968d2ae6deff5
Takayama Fumihiko <tekezo@pqrs.org>
2026-03-09 22:27:29 +0900

    http -> https

--- include/pqrs/osx/iokit_types.hpp
+++ include/pqrs/osx/iokit_types.hpp
@@ -4,7 +4,7 @@
 
 // (C) Copyright Takayama Fumihiko 2018.
 // Distributed under the Boost Software License, Version 1.0.
-// (See http://www.boost.org/LICENSE_1_0.txt)
+// (See https://www.boost.org/LICENSE_1_0.txt)
 
 #include "iokit_types/hash.hpp"
 #include "iokit_types/iokit_hid_location_id.hpp"
